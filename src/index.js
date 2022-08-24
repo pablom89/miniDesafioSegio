29eaa2fe2a99070826259224549cfe3ef94bb0bb
pablom89 <pablo_89_cba@hotmail.com>
2022-08-24 14:52:26 -0300

    1 file modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './custom.scss';
 
 import App from './App';
 
